1f04a9ff69fcc91b67c8b0f2c7a2fcef74d59845
Mohan-12365 <mohanprasanthd01@gmail.com>
2026-03-18 00:38:12 +0530

    Update CorsConfig.java

---
+++ coconut-management/src/main/java/com/coconut/coconut_management/config/CorsConfig.java
@@ -0,0 +1,22 @@
+package com.coconut.coconut_management.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.*;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://secure-login-hxjn7im0b-mohan-prasanths-projects.vercel.app")
+                        .allowedMethods("*")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
