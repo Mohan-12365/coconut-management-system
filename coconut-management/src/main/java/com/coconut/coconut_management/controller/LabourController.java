0337fb00fffcabe579f11541818cd4fbc4f5c303
Mohan-12365 <mohanprasanthd01@gmail.com>
2026-03-19 00:20:22 +0530

    Update LabourController.java

--- coconut-management/src/main/java/com/coconut/coconut_management/controller/LabourController.java
+++ coconut-management/src/main/java/com/coconut/coconut_management/controller/LabourController.java
@@ -8,7 +8,7 @@ import java.util.List;
 import com.coconut.coconut_management.entity.Labour;
 import com.coconut.coconut_management.repository.LabourRepository;
 
-@CrossOrigin(origins = "http://localhost:3000")
+
 @RestController
 @RequestMapping("/labours")
 public class LabourController {
