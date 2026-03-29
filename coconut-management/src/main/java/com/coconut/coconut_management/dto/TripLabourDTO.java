60814c0df1d5b13defd01a19f6ce66b02f4f386f
Mohan-12365 <mohanprasanthd01@gmail.com>
2026-03-29 20:04:26 +0530

    Update TripLabourDTO.java

--- coconut-management/src/main/java/com/coconut/coconut_management/dto/TripLabourDTO.java
+++ coconut-management/src/main/java/com/coconut/coconut_management/dto/TripLabourDTO.java
@@ -1,1 +1,23 @@
 
+package com.coconut.coconut_management.dto;
+
+public class TripLabourDTO {
+
+    private String name;
+    private String phone;
+    private double wage;
+    private boolean driver;
+
+    public TripLabourDTO(String name, String phone, double wage, boolean driver) {
+        this.name = name;
+        this.phone = phone;
+        this.wage = wage;
+        this.driver = driver;
+    }
+
+    // getters
+    public String getName() { return name; }
+    public String getPhone() { return phone; }
+    public double getWage() { return wage; }
+    public boolean isDriver() { return driver; }
+}
