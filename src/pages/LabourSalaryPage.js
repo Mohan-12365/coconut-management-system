c3394e2a96cd9a89f60173ab0a78d07f71783ca3
Mohan-12365 <mohanprasanthd01@gmail.com>
2026-04-02 21:52:08 +0530

    Create LabourSalaryPage.js

---
+++ src/pages/LabourSalaryPage.js
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+import api from "../api";
+
+function LabourSalaryPage() {
+
+  const [salaryData, setSalaryData] = useState(null);
+
+  const labourId = localStorage.getItem("labourId");
+
+  useEffect(() => {
+    api.get(`/salary/weekly/${labourId}?startDate=2026-03-01&endDate=2026-03-31`)
+      .then(res => setSalaryData(res.data))
+      .catch(err => console.log(err));
+  }, []);
+
+  return (
+    <div className="container mt-4">
+      <h2>My Salary</h2>
+
+      {salaryData && (
+        <div className="card p-3">
+          <p>Total Wage: ₹{salaryData.totalWages}</p>
+          <p>Expense: ₹{salaryData.totalExpense}</p>
+          <h4>Final Salary: ₹{salaryData.finalSalary}</h4>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default LabourSalaryPage;
