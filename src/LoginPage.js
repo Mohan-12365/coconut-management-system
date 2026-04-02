f8c54bcc3183ba8d5a4315471de783503bb8cd3b
Mohan-12365 <mohanprasanthd01@gmail.com>
2026-04-02 23:01:44 +0530

    Update LoginPage.js

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -33,7 +33,7 @@ function LoginPage({ onLogin }) {
     })
     .then(res => {
       alert("Login Success");
-      localStorage.setItem("token", JSON.stringify(res.data));
+      //localStorage.setItem("token", JSON.stringify(res.data));
       localStorage.setItem("token", res.data.token);
       localStorage.setItem("role", res.data.role);
       localStorage.setItem("labourId", res.data.labourId); // 🔥 important
