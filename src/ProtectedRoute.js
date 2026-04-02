11128ed30a892744945ad8c290b05e76b4022404
Mohan-12365 <mohanprasanthd01@gmail.com>
2026-04-02 22:27:09 +0530

    Update ProtectedRoute.js

--- src/ProtectedRoute.js
+++ src/ProtectedRoute.js
@@ -1,11 +1,17 @@
 import { Navigate } from "react-router-dom";
 
-function ProtectedRoute({ children }) {
+function ProtectedRoute({ children , allowedRole}) {
   const token = localStorage.getItem("token");
+   const role = localStorage.getItem("role");
 
   if (!token) {
     return <Navigate to="/" />;
   }
+
+   if (allowedRole && role !== allowedRole) {
+    return <Navigate to="/" />;
+  }
+
 
   return children;
 }
