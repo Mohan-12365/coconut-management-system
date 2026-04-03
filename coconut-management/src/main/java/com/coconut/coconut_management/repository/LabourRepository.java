c4de86ce1df8318c26b232130173de28866ef909
Mohan-12365 <mohanprasanthd01@gmail.com>
2026-04-03 22:33:30 +0530

    Update LabourRepository.java

--- coconut-management/src/main/java/com/coconut/coconut_management/repository/LabourRepository.java
+++ coconut-management/src/main/java/com/coconut/coconut_management/repository/LabourRepository.java
@@ -1,5 +1,6 @@
 package com.coconut.coconut_management.repository;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.coconut.coconut_management.entity.Labour;
 
