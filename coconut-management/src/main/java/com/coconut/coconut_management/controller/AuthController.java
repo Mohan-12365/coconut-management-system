464b24ab468006e63f57f545eca48b37397d776b
Mohan-12365 <mohanprasanthd01@gmail.com>
2026-04-03 22:49:52 +0530

    Update AuthController.java

--- coconut-management/src/main/java/com/coconut/coconut_management/controller/AuthController.java
+++ coconut-management/src/main/java/com/coconut/coconut_management/controller/AuthController.java
@@ -1,15 +1,12 @@
 package com.coconut.coconut_management.controller;
 
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.coconut.coconut_management.repository.LabourRepository;
 import com.coconut.coconut_management.entity.Labour;
+import com.coconut.coconut_management.dto.*;
 
 import java.util.Optional;
 
