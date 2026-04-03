bb819325d2d13bffeb00bd78a4e5a8777327cd3c
Mohan-12365 <mohanprasanthd01@gmail.com>
2026-04-03 22:32:37 +0530

    Update AuthController.java

--- coconut-management/src/main/java/com/coconut/coconut_management/controller/AuthController.java
+++ coconut-management/src/main/java/com/coconut/coconut_management/controller/AuthController.java
@@ -1,3 +1,5 @@
+package com.coconut.coconut_management.controller;
+
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.CrossOrigin;
