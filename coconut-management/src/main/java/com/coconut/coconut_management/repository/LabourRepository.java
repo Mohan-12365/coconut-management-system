24d142607b47a75014aedbbb3786028411989743
Mohan-12365 <mohanprasanthd01@gmail.com>
2026-04-03 22:01:33 +0530

    Update LabourRepository.java

--- coconut-management/src/main/java/com/coconut/coconut_management/repository/LabourRepository.java
+++ coconut-management/src/main/java/com/coconut/coconut_management/repository/LabourRepository.java
@@ -5,4 +5,5 @@ import com.coconut.coconut_management.entity.Labour;
 
 public interface LabourRepository extends JpaRepository<Labour, Long> {
 
+  Optional<Labour> findByUsername(String username);
 }
