277834f6130badd76ad3cab47210636cb4936885
Mohan-12365 <mohanprasanthd01@gmail.com>
2026-04-05 13:46:45 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -121,7 +121,7 @@ function App() {
           <Route path="/add-vehicle" element={<ProtectedRoute allowedRole="ADMIN"><AddVehiclePage/></ProtectedRoute>} />
           <Route path="/labours" element={<ProtectedRoute allowedRole="ADMIN"><LabourListPage/></ProtectedRoute>} />
           <Route path="/edit-labour/:id" element={<ProtectedRoute allowedRole="ADMIN"><EditLabourPage/></ProtectedRoute>} />
-          // <Route path="/dashboard" element={<ProtectedRoute><DashboardPage/></ProtectedRoute>} /> 
+        {/* <Route path="/dashboard" element={<ProtectedRoute><DashboardPage/></ProtectedRoute>} /> */}
             <Route path="/my-salary" element={<ProtectedRoute allowedRole="LABOUR"><LabourSalaryPage/></ProtectedRoute>} />
             <Route path="/my-expense" element={<ProtectedRoute allowedRole="LABOUR"><LabourExpensePage/></ProtectedRoute>} />
         </Routes>
