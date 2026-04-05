89dae73777730d47a0e054e662824aca2eeee3b8
Mohan-12365 <mohanprasanthd01@gmail.com>
2026-04-05 13:33:46 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@
 //import {useState} from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import TripPage from "./pages/TripPage";
-//import SalaryPage from "./pages/SalaryPage";
+import SalaryPage from "./pages/SalaryPage";
 import DashboardPage from "./pages/DashboardPage";
 //import TripHistory from "./TripHistory";
-import SalaryReport from "./SalaryReport";
+//import SalaryReport from "./SalaryReport";
 import LoginPage from "./LoginPage";
 import ProtectedRoute from "./ProtectedRoute";
 import ExpensePage from "./pages/ExpensePage";
